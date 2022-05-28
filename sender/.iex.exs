64b381a731ed615a1c0f18bfe063c54db542534a
girorme <rodrigogirorme@gmail.com>
2022-05-28 01:46:57 -0300

    Add sender email with async_stream example

---
+++ sender/.iex.exs
@@ -0,0 +1,6 @@
+emails = [
+  "hello@world.com",
+  "hola@world.com",
+  "nihao@world.com",
+  "konnichiwa@world.com",
+]
--- sender/lib/sender.ex
+++ sender/lib/sender.ex
@@ -15,4 +15,16 @@ defmodule Sender do
   def hello do
     :world
   end
+
+  def notify_all(emails) do
+    emails
+    |> Task.async_stream(&send_email/1, ordered: false)
+    |> Enum.to_list()
+  end
+
+  def send_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
 end
