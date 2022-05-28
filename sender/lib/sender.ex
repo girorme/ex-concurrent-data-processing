d6458b9cd67dff56fbdebf0c37996c4db71b8034
girorme <rodrigogirorme@gmail.com>
2022-05-28 01:07:49 -0300

    Create sender project

---
+++ sender/lib/sender.ex
@@ -0,0 +1,18 @@
+defmodule Sender do
+  @moduledoc """
+  Documentation for `Sender`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Sender.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
