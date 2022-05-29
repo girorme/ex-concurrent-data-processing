975136924ceb05c9966adb58858b6809b285d231
girorme <rodrigogirorme@gmail.com>
2022-05-29 01:58:22 -0300

    Add jobber project

---
+++ jobber/lib/jobber.ex
@@ -0,0 +1,18 @@
+defmodule Jobber do
+  @moduledoc """
+  Documentation for `Jobber`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jobber.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
