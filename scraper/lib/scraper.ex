c5c6ac43858708bd9b962dce63c5a112a2a43ebc
girorme <rodrigogirorme@gmail.com>
2022-06-04 22:23:13 -0300

    Add scraper project

---
+++ scraper/lib/scraper.ex
@@ -0,0 +1,18 @@
+defmodule Scraper do
+  @moduledoc """
+  Documentation for `Scraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Scraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ scraper/lib/scraper/application.ex
@@ -0,0 +1,20 @@
+defmodule Scraper.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Starts a worker by calling: Scraper.Worker.start_link(arg)
+      # {Scraper.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Scraper.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
