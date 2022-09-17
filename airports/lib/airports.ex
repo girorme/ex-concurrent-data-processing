c98eb206748719e6d163c70d77b512438519a3e5
girorme <rodrigogirorme@gmail.com>
2022-09-17 00:36:05 -0300

    project init

---
+++ airports/lib/airports.ex
@@ -0,0 +1,18 @@
+defmodule Airports do
+  @moduledoc """
+  Documentation for `Airports`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Airports.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
