a3a6035bc552589b3fcb261ec8b9e2ceff839f3c
girorme <rodrigogirorme@gmail.com>
2022-09-17 01:19:16 -0300

    add airports open with stream

--- airports/lib/airports.ex
+++ airports/lib/airports.ex
@@ -1,18 +1,23 @@
 defmodule Airports do
-  @moduledoc """
-  Documentation for `Airports`.
-  """
+  alias NimbleCSV.RFC4180, as: CSV
 
-  @doc """
-  Hello world.
+  def airports_csv() do
+    Application.app_dir(:airports, "/priv/airports.csv")
+  end
 
-  ## Examples
-
-      iex> Airports.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def open_airports() do
+    airports_csv()
+    |> File.stream!()
+    |> CSV.parse_stream()
+    |> Stream.map(fn row ->
+      %{
+        id: :binary.copy(Enum.at(row, 0)),
+        type: :binary.copy(Enum.at(row, 2)),
+        name: :binary.copy(Enum.at(row, 3)),
+        country: :binary.copy(Enum.at(row, 8))
+      }
+    end)
+    |> Stream.reject(&(&1.type == "closed"))
+    |> Enum.to_list()
   end
 end
